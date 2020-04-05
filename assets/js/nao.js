4bddbc384d8dea0032482d5a11a9f2eeefec3e78
makeupwith <g1644303@cc.kyoto-su.ac.jp>
2020-04-05 13:25:35 +0900

    Add cookie processing

--- assets/js/nao.js
+++ assets/js/nao.js
@@ -1,27 +1,64 @@
+// 初期値
 var ipAddress = "10.0.1.2";
 
-$.get("https://ipinfo.io", function(res){
-  ipAddress = res.ip;
-}, "jsonp");
+// # cookieの設定
+var limite = 1; //Cookieの期限（1日）
+var nowdate = new Date(); //現在の日付データを取得
+nowdate.setTime(nowdate.getTime() + limite*24*60*60*1000); 
+var deadline = nowdate.toGMTString(); //GMT形式に変換して変数kigendateに格納
+var expires = "expires=" + deadline + "; ";
+var path = "path=/";
+var dt = new Date('1999-12-31T23:59:59Z'); // 過去の日付をGMT形式に変換
 
+// Winodwがロードされた際
 window.onload = function() {
-  var ip = document.getElementById('ip');
-  ip.innerHTML = ipAddress;
+  var session;
 
-  var session = new QiSession(ipAddress);
-  
+  // # cookieにIPアドレスが保存されていた場合、NAOとの接続を行う
+  if(this.navigator.cookieEnabled) {
+    console.log("cookieEnabled");
+    if(document.cookie.indexOf("ip_nao") == -1) {
+      console.log("nothing input ip address");
+      session = new QiSession(ipAddress)
+    }else {
+      var cookies = document.cookie.split("; ");
+      for(var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].split("=");
+        switch(cookie[0]) {
+          case "ip_nao" :
+            ip_nao = cookie[1];
+            console.log(ip_nao);
+            session = new QiSession(ip_nao)
+            var ip_input = document.getElementById('ip_nao');
+            ip_input.innerHTML = ip_nao + " (以前の入力)";
+            break;
+        }
+      }
+    } 
+  }
+
+  // # NAOと接続
+  session.socket().on('connect', function() {
+    console.log("[CONNECTED]");
+    var connect = document.getElementById('connect');
+    connect.innerHTML = "接続中";
+  }).on('disconnect', function() {
+    console.log("[DISCONNECTED]");
+    var connect = document.getElementById('connect');
+    connect.innerHTML = "接続断"
+  }).on('error', function() {
+    console.log("[CONNECTION ERROR]")
+    var connect = document.getElementById('connect');
+    connect.innerHTML = "接続エラー"
+  });
+
+  // # IPアドレスの入力時
   $('#ip-btn').on('click', function() {
     var naoIp = $('#nao_ip').val();
     session = new QiSession(naoIp);
-    console.log(naoIp);
-  });
-  
-  // var session = new QiSession("10.0.1.2");
-
-  session.socket().on('connect', function() {
-      console.log("connected!");
-      var connect = document.getElementById('connect');
-      connect.innerHTML = "接続中";
+    // ## IPアドレスをcookieに保存
+    var cookievalue = "ip_nao=" + naoIp + "; ";
+    document.cookie = cookievalue + expires + path; 
   });
 
 
@@ -113,6 +150,5 @@ window.onload = function() {
       console.log("Error!");
     });
   }); 
-
 
 };  
